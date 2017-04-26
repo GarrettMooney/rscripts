9533d8a84216ff33f3b5b543964113c51a04ad36
Garrett Mooney <gmooney@mail.bradley.edu>
2017-04-25 20:47:21 -0400

    Updated infix to include magrittr and lubridate functions

--- infix_utils.R
+++ infix_utils.R
@@ -1,6 +1,16 @@
-'%nin%' <- Negate('%in%')
-'%in_v%' <- function(x, y) x[x %in% y] 
-'%nin_v%' <- function(x, y) x[!x %in% y] 
-'%in_d%' <- function(x, y) x[names(x) %in% y] 
-'%nin_d%' <- function(x, y) x[!names(x) %in% y] 
-'%+%' <- function(x, y) paste0(x, y)
+## Set operators & misc
+`%nin%` <- Negate(`%in%`)
+`%in_v%` <- function(x, y) x[x %in% y] 
+`%nin_v%` <- function(x, y) x[!x %in% y] 
+`%in_d%` <- function(x, y) x[names(x) %in% y] 
+`%nin_d%` <- function(x, y) x[!names(x) %in% y] 
+`%+%` <- function(x, y) paste0(x, y)
+
+## magrittr pipe
+`%>%` <- magrittr::`%>%`
+`%<>%` <- magrittr::`%<>%`
+`%T>%` <- magrittr::`%T>%`
+
+## Time operators
+`%m+%` <- lubridate::`%m+%`
+`%m-%` <- lubridate::`%m-%`
